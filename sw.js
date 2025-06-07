6f5355298f611d9a5dec8a0e18625155e01217ea
MangoFrogaparte <devinbyers216@gmail.com>
2025-06-07 12:54:07 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+// In your main script tag or a separate JS file
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
